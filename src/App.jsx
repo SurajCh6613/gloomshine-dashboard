8204ee10c40f797fc9bd7eec48452ccdfa92c863
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-08-30 11:35:20 +0530

    Hero Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./components/NotFound";
 import Dashboard from "./pages/Dashboard";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}>
+        <Route path="/" element={<Hero />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Route>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex min-h-screen justify-center items-center text-3xl lg:text-6xl text-gray-600'>Hero Page </div>
+  )
+}
+
+export default Hero
